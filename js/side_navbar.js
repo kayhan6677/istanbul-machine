527fedd8249d7e7397cc0a06faee897a84d12205
Kayhan Aykın <kayhan.aykin@gmail.com>
2023-02-27 14:03:29 +0300

    commit 3

--- js/side_navbar.js
+++ js/side_navbar.js
@@ -3,9 +3,8 @@ const styles = getComputedStyle(btn);
 let left = styles.getPropertyValue("left");
 
 
-document.getElementById("navbar_button").onclick = function() {myFunction()};
+document.getElementById("navbar_button").onclick = function() {
 
-function myFunction () {
   if (left == "0px") {
     document.getElementById("main_container").style.left = "50%";
     document.getElementById("main_container").style.padding = "0 0 0 15px";
@@ -17,13 +16,14 @@ function myFunction () {
     document.getElementById("left_side_container").style.left = "-50%";
     left = "0px";
   }
+
 };
 
-document.getElementById("sidebar_button").onclick = function() {myFunction2()};
+document.getElementById("sidebar_button").onclick = function() {
 
-function myFunction2 () {
     document.getElementById("main_container").style.left = "0px";
     document.getElementById("main_container").style.padding = "0 0 0 0";
     document.getElementById("left_side_container").style.left = "-50%";
     left = "0px";
-};
+
+  };
---
+++ js/slide_show.js
@@ -0,0 +1,60 @@
+let clickCount = 0;
+let clickCheck = 0;
+const width = -241.5;
+
+function forwardFunction () {
+    clickCount++;
+    if(clickCount > 2) {
+        clickCount = 0;
+        clickCheck = 0;
+    }
+
+    translateX = clickCount * width;
+
+    document.getElementById("we_manufacture_slide_container_inner").style.transform = 
+    'translateX(' + translateX+ 'px)';
+    
+    if(clickCheck == 1)  {
+        document.getElementById("we_manufacture_slide_container_inner").style.transition = "200ms ease";
+        clickCheck = 0;
+        } else {
+        document.getElementById("we_manufacture_slide_container_inner").style.transition = "1000ms ease";
+        }
+}
+
+document.getElementById("greater_than").onclick = function() {
+    clickCheck = 1;
+    forwardFunction ()
+    clearInterval (intervalID)
+    intervalID = setInterval (forwardFunction, 2000);
+};
+
+
+document.getElementById("lower_than").onclick = function() {
+    clickCount--;
+    if(clickCount < 0) {
+        clickCount = 2;
+        clickCheck = 0;
+    }
+
+    translateX = clickCount * width;
+
+    document.getElementById("we_manufacture_slide_container_inner").style.transform = 
+    'translateX(' + translateX+ 'px)';
+   
+    if(clickCheck == 0) {
+        document.getElementById("we_manufacture_slide_container_inner").style.transition = "200ms ease";
+        } else {
+        document.getElementById("we_manufacture_slide_container_inner").style.transition = "1000ms ease";
+        fastSlide = 0;
+        } 
+    clearInterval (intervalID)
+    intervalID = setInterval (forwardFunction, 2000);
+}
+
+var intervalID = setInterval (forwardFunction, 2000);
+
+
+
+
+    
